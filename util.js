e35966aa3f850de2ba32c88005dd06a1d994c702
Niels Koster <n.koster@portavita.eu>
2020-10-26 14:47:20 +0100

    Complete getPostData helper

--- util.js
+++ util.js
@@ -1,11 +1,27 @@
 const fs = require('fs')
 
 const writeDataToFile = (filename, content) => {
-    fs.writeFileSync(filename, JSON.stringify(content), 'utf8', error => {
-        console.log(error)
+    fs.writeFileSync(filename, JSON.stringify(content), 'utf8', err => {
+        console.log(err)
+    })
+}
+
+const getPostData = req => {
+    return new Promise((yes, no) => {
+        try {
+            let body = ''
+            req.on('data', chunk => {
+                body += chunk.toString()
+            })
+            req.on('end', _ => {
+                yes(body)
+            })
+        } catch (err) {
+            no(err)
+        }
     })
 }
 
 module.exports = {
-    writeDataToFile
+    writeDataToFile, getPostData
 }
