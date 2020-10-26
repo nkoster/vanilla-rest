c5cfb3c0afb0a5334835cc34a07d2aeaa09126c9
Niels Koster <n.koster@portavita.eu>
2020-10-26 14:51:21 +0100

    Add updateProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/productModels')
+const { getPostData } = require('../util')
 
 // GET /api/products
 const getProducts = async (req, res) => {
@@ -31,7 +32,6 @@ const getProduct = async (req, res, id) => {
             })
             res.end(JSON.stringify(product))
         }
-
         res.end(JSON.stringify(product))  
     } catch (error) {
         console.log(error)
@@ -59,8 +59,37 @@ const createProduct = async (req, res) => {
     }
 }
 
+// PUT /api/products/:id
+const updateProduct = async (req, res, id) => {
+    try {
+        const product = await Product.findById(id)
+        if (!product) {
+            res.writeHead(404, {
+                'Content-Type': 'application/json',
+                'Owned-By': 'Niels-da-Piels'
+            })
+            res.end(JSON.stringify({message: `Product ${id} not found`}))
+        } else {
+            const body = await getPostData(req)
+            console.log('BODY', body)
+            const { title, description, price } = JSON.parse(body)
+            const productData = {
+                title: title || product.title,
+                description: description || product.description,
+                price: price || product.price
+            }
+            updatedProduct = await Product.update(id, productData)
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Owned-By': 'Niels-da-Piels'                
+            })
+            res.end(JSON.stringify(updatedProduct))
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 module.exports = {
-    getProducts,
-    getProduct,
-    createProduct
+    getProducts, getProduct, createProduct, updateProduct
 }
