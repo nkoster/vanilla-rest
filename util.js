53db5eb7c75f512ccbe55cd3b564bc8877cbc3ba
Niels Koster <n.koster@portavita.eu>
2020-10-26 20:53:24 +0100

    Remove uuid dependency

--- util.js
+++ util.js
@@ -27,6 +27,16 @@ const response = (res, status, payload) => {
     res.end(payload)    
 }
 
+// Below is not optimal, careful.
+const uid = _ => {
+    return (Math.random() * Date.now()).toString(16).substr(0, 8) + '-' +
+    (Math.random() * Date.now()).toString(16).substr(0, 4) + '-' +
+    (Math.random() * Date.now()).toString(16).substr(0, 4) + '-' +
+    (Math.random() * Date.now()).toString(16).substr(0, 4) + '-' +
+    (Math.random() * Date.now()).toString(16).substr(0, 6) +
+    (Math.random() * Date.now()).toString(16).substr(0, 6)
+}
+
 module.exports = {
-    writeDataToFile, getPostData, response
+    writeDataToFile, getPostData, response, uid
 }
