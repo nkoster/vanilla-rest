159ae6a8e0c97a7f70a4f2131eb0286802a6af0f
Niels Koster <n.koster@portavita.eu>
2020-10-26 16:45:00 +0100

    Allow a-z0-9

--- server.js
+++ server.js
@@ -7,15 +7,15 @@ const {
 const server = http.createServer((req, res) => {
     if (req.url === '/api/products' && req.method === 'GET') {
         getProducts(req, res)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
+    } else if (req.url.match(/\/api\/products\/([a-z0-9]+)/) && req.method === 'GET') {
         const id = req.url.split('/')[3]
         getProduct(req, res, id)
     } else if (req.url === '/api/products' && req.method === 'POST') {
         createProduct(req, res)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    } else if (req.url.match(/\/api\/products\/([a-z0-9]+)/) && req.method === 'PUT') {
         const id = req.url.split('/')[3]
         updateProduct(req, res, id)
-    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    } else if (req.url.match(/\/api\/products\/([a-z0-9]+)/) && req.method === 'DELETE') {
         const id = req.url.split('/')[3]
         deleteProduct(req, res, id)
     } else {
